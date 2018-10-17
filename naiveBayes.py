46b51c316d0b62895fcb6c60c82184c1f49ee7a5
loganpadon <logan.padon@gmail.com>
2018-10-17 00:44:31 -0500

    Imports

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,3 +1,8 @@
-import nltk
-import pandas
-import numpy
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from math import log, sqrt
+import pandas as pd
+import numpy as np
+
+
